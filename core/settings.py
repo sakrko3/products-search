57c16d908b317d1a6e9bdda647e6b0d2d4e50329
sakrko3 <saikrishnak930@gmail.com>
2023-01-18 12:19:56 +0530

    Imported os

--- core/settings.py
+++ core/settings.py
@@ -10,11 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 from decouple import Csv, config
 from dj_database_url import parse as db_url
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
